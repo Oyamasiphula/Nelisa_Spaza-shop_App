f22717bcc8401bdd144f6caf62aed2913b76a0f2
oyama <oyama@projectcodex.co>
2015-07-22 12:31:06 +0200

    file that will requires my functions to check if get sales total costs function is working corectly

--- runFile.js
+++ runFile.js
@@ -1,8 +1,11 @@
 var csvRead = require('./methods/readCSV');
 var qtyPerProduct = require('./methods/productQty');
+var salesCosts = require('./methods/salesCost');
 
 var salesData = csvRead.getData("./data/NelisaSalesHistory.csv");
 //console.log(salesData);
 var prodQty = qtyPerProduct.getProductQty(salesData);
 console.log(prodQty);
 
+var totalCostPerProducts = salesCosts.getSaleCostsPerProduct(salesData);
+console.log(totalCostPerProducts);
