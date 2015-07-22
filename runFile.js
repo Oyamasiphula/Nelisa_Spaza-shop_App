a1d3832960621542953ceb68c774c046a9258418
oyama <oyama@projectcodex.co>
2015-07-22 12:25:19 +0200

    file that will requires my functions to check if get quantity function is working corectly

--- runFile.js
+++ runFile.js
@@ -1,4 +1,8 @@
 var csvRead = require('./methods/readCSV');
+var qtyPerProduct = require('./methods/productQty');
 
 var salesData = csvRead.getData("./data/NelisaSalesHistory.csv");
-console.log(salesData);
+//console.log(salesData);
+var prodQty = qtyPerProduct.getProductQty(salesData);
+console.log(prodQty);
+
