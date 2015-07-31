96b41c3d5529d730d0f3f0514060a092383ed61c
oyama <oyama@projectcodex.co>
2015-07-31 11:44:42 +0200

    THIS FILE stil need to be fixed

--- methods/totalSalesPerCategory.js
+++ methods/totalSalesPerCategory.js
@@ -1,6 +1,6 @@
 exports.getSalesPerProductsCategory = function(salesLines){
 		// var qtyPerCategoryMap = {};
-		var qtyPerProduct = [];
+		 var qtyPerProduct = [];
 	    var productsSold = {};
     salesLines.forEach(function(line){
             //split each line into fields
@@ -15,44 +15,7 @@ exports.getSalesPerProductsCategory = function(salesLines){
           productsSold[productName] =productsSold[productName] + Number(qty);  
              
         });
-    		console.log(qtyPerProduct);
+    		console.log(productsSold);
     		qtyPerProduct.push(productsSold);
-    		return productsSold
-
-   //  		var productCategories = {
-			// 					    'Fanta 500ml' : 'Beverages',
-			// 					    'Coke 500ml' : 'Beverages',
-			// 					    'Cream Soda 500ml' : 'Beverages',
-			// 					    'Shampoo 1 litre' : 'Toiletries',
-			// 					    'Soap Bar' : 'Toiletries',
-			// 					    'Mixed Sweets 5s' : 'Luxuries',
-			// 					    'Heart Chocolates' : 'Luxuries',
-			// 					    'Iwisa Pap 5kg' : 'Long_Life_Groceries',
-			// 					    'Top Class Soy Mince' : 'Long_Life_Groceries',
-			// 					    'Bread' : 'Short_Life_Groceries',
-			// 					    'Milk 1l' : 'Short_Life_Groceries',
-			// 					    'Imasi' : 'Short_Life_Groceries',
-			// 					    'Rose (plastic)' : 'Novelty_Goods',
-			// 					    'Valentine Cards' : 'Novelty_Goods',
-			// 					    'Bananas - loose' : 'Fruit',
-			// 					    'Apples - loose' : 'Fruit',
-			// 					    'Chakalaka Can' : 'Tinned_Food',
-			// 					    'Gold Dish Vegetable Curry Can' : 'Tinned_Food'
-			// 					};
-
-			// for(var productName in salesLines){
-			       
-			//         var qty = qtyPerProduct[productName];
-			//         var categoryName = productCategories[productName];
-
-			//         if(qtyPerCategoryMap[categoryName] === undefined){
-			//             qtyPerCategoryMap[categoryName] = 0;
-			//         };
-			//         qtyPerCategoryMap[categoryName] = qtyPerCategoryMap[categoryName] + Number(qty);
-			//     }
-			//     console.log('=========================================================== below - Popular CategoryName + Qty PerCategory map');
-			//     console.log(qtyPerCategoryMap);
-			//     console.log('=========================================================== end of CategoryName + Qty PerCategory map');
-			// 	return qtyPerCategoryMap;
-
+    		return productsSold;
 	};
